ec235c246c60c38e57d6b1f8f1ff027e77278d29
wesleigit <ufothec@hotmail.com>
2019-04-01 20:15:07 -0300

    criacao da classe Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos); 
 
 	}
 
