221d44ebc28a1c07f5f63bc48809a867749587b2
wesleigit <ufothec@hotmail.com>
2019-04-01 22:19:48 -0300

    imprimir pecas do tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,13 @@
 package aplicacao;
 
-import tabuleiro.Posicao;
+import xadrez.PartidaDeXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Posicao pos = new Posicao(3, 5);
-		System.out.println("p" + pos); 
+		
+		PartidaDeXadrez partidaDeXadres = new PartidaDeXadrez();
+		UI.imprimirTabuleiro(partidaDeXadres.getPecas());
 
 	}
 
