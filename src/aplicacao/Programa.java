03151ff2cf1c0f5d552df23091bd92c052770457
wesleigit <ufothec@hotmail.com>
2019-04-01 21:10:06 -0300

    Ciado classe Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,19 +1,12 @@
 package aplicacao;
 
-<<<<<<< HEAD
 import tabuleiro.Posicao;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos); 
-=======
-public class Programa {
-
-	public static void main(String[] args) {
-		System.out.println("ola mundo");
->>>>>>> 1326365a058406b7433079df1971561f24ea1e07
+		System.out.println("p" + pos); 
 
 	}
 
